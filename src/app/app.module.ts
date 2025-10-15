55db737e441123f334072e9d9ad8832dd0cad560
black3snake <black3snake@mail.ru>
2025-10-15 10:55:11 +0800

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './views/main/main.component';
+import { LayoutComponent } from './shared/layout/layout.component';
+import {SharedModule} from "./shared/shared.module";
+import {FooterComponent} from "./shared/footer/footer.component";
+import {HeaderComponent} from "./shared/header/header.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    LayoutComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
+    SharedModule,
     BrowserModule,
     AppRoutingModule
   ],
